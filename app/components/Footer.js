7b4f2a8fdfdd6b094f52f94f1dbce463cd843895
NuntichaThummee <nunticha.ppalaloy909@gmail.com>
2025-03-26 02:33:47 -0400

    styling header, footer and home page

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,24 +1,23 @@
 import Link from "next/link";
+import styles from "./page.module.css";
 
 export default function Footer() {
     return (
-        <footer>
-            <div>
-                <ul>
+        <footer className={styles.containerFooter}>
+                <ul className={styles.nav}>
                     {/* change it later*/}
                     <li><Link href="/">About Us</Link></li>
                     <li><Link href="/">Team</Link></li>
                     <li><Link href="/">Policy</Link></li>
                     <li><Link href="/">Feedback</Link></li>
                 </ul>
-                <ul>
+                <ul className={styles.nav}>
                     {/* change icon later*/}
                     <li><Link href="/fb">🔍</Link></li>
                     <li><Link href="/ig">🔍</Link></li>
                     <li><Link href="/x">🔍</Link></li>
                 </ul>
                 <p>© 2025 Booklect. All Rights Reserved.</p>
-            </div>
         </footer>
     );
 }
--- app/page.js
+++ app/page.js
@@ -2,23 +2,26 @@ import Link from "next/link";
 import Image from "next/image";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import styles from "./components/page.module.css";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <main>
-        <div>
-          <h1>Welcome to Booklect <br />
-            Your Virtual Bookshelf!</h1>
-          <p>Booklect helps you organize, track, and discover books with ease. Keep tabs on what you’ve read, what you’re reading, and what’s next, all in
-            one place. </p>
-          <h4>Start your reading journey today!</h4>
-        </div>
-        <div>
-          <Image src="/reading.png" alt="kids reading the book" width={604} height={480} priority />
-        </div>
-        <Link href="/SearchBooks"><button type="button">Get Start!</button></Link>
+        <section className={styles.containerHome}>
+          <div>
+            <h1>Welcome to Booklect <br />
+              Your Virtual Bookshelf!</h1>
+            <p>Booklect helps you organize, track, and discover books with ease. Keep tabs on what you’ve read, what you’re reading, and what’s next, all in
+              one place. </p>
+            <h4>Start your reading journey today!</h4>
+            <Link href="/SearchBooks"><button type="button">Get Start!</button></Link>
+          </div>
+          <div>
+            <Image src="/reading.png" alt="kids reading the book" width={604} height={480} priority />
+          </div>
+        </section>
       </main>
       <Footer />
     </div>
