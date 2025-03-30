0fa39ff7a267a3d1a3fd4db4354b36b942684abf
NuntichaThummee <nunticha.ppalaloy909@gmail.com>
2025-03-30 01:07:17 -0400

    add logo

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -7,17 +7,21 @@ export default function Header() {
         <header className="containerHeader" >
             {/* change text to logo*/}
             <div>
-                Booklect.
+                <Image src="/booklect_logo2.png" alt="booklect logo" width={190} height={67} priority />
             </div>
             <nav>
                 <ul className="nav">
                     <li><Link href="/">Home</Link></li>
-                    <li><Link href="/Dashboard">Dashboard</Link></li>
-                    <li><Link href="/MyBooks">My books</Link></li>
+                    <li><Link href="/">Dashboard</Link></li>
+                    <li><Link href="/">My books</Link></li>
                     {/* change emoji to icon later*/}
                     <li className="navIcon"><Link href="/SearchBooks"><Image src="/search.png" alt="search icon" width={24} height={24} priority /></Link></li>
                 </ul>
             </nav>
-        </header>
+        </header >
     );
 }
+
+
+
+
