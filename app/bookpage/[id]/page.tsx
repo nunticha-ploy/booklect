2eb1689c7fb155a9abf7240b533e46d9e7edebb1
OlgaTimoshenkoSecE <otim0904@gmail.com>
2025-03-30 14:13:28 -0400

    commit
    just committing bc it asks me to

--- app/bookpage/[id]/page.tsx
+++ app/bookpage/[id]/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import React from 'react';
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Header from "./components/Header.tsx";
+import Footer from "./components/Footer.tsx";
 
 function Bookpage(){
     return(
