4c3bfb375233ca2bb40e90c66731e9c3db49e9c5
OlgaTimoshenkoSecE <otim0904@gmail.com>
2025-03-30 14:37:01 -0400

    aaarrhhh

---
+++ app/bookdetails.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import "./components/page.css";
+
+export default function Bookpage(){
+    return(
+        <div>
+            <Header/>
+            <Footer/>
+        </div>
+    )
+}
