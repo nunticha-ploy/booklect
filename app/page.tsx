10bc478c7e608d346e17efe026dec047ac70d8c1
NuntichaThummee <nunticha.ppalaloy909@gmail.com>
2025-03-30 23:40:34 -0400

    import globals.css

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import "./components/page.css";
+import './globals.css';
 
 export default function Home() {
   return (
