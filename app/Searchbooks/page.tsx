7e735f692242e6a0742777b160a88be12f0ebd91
NuntichaThummee <nunticha.ppalaloy909@gmail.com>
2025-04-08 01:49:03 -0400

    create GetBook
    fetch API and create search function

--- app/Searchbooks/page.tsx
+++ app/Searchbooks/page.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import GetBook from "../components/GetBook";
 import "../components/page.css";
+import '../globals.css';
 
 export default function SearchPage () {
     return (
@@ -10,6 +12,7 @@ export default function SearchPage () {
           <main>
             <section className="containerSearch">
                 <h1>Search Book</h1>
+                <GetBook />
             </section>
           </main>
           <Footer />
